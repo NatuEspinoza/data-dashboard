05d1ca2f2d76d7e242fd19eb349f6b2ae7210bd0
NatuEspinoza <natalia.utemdcv@gmail.com>
2017-11-18 13:49:23 -0300

    maquetación inicial html/css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
-/*
- * Funcionalidad de tu producto
- */
+window.onload = function(){
+  // menu lateral de usuario
+  var addStudent = document.getElementById('addStudent');
+  addStudent.addEventListener('click', function() {
+
+  });
+  var removeStudent = document.getElementById('removeStudent');
+  removeStudent.addEventListener('click', function() {
+
+  });
+  var addSprint = document.getElementById('addSprint');
+  addSprint.addEventListener('click', function() {
+
+  });
+
+  //Al hacer click en sede y generación
+  var filtroSedes = document.getElementById('sedes');
+  filtroSedes.addEventListener('click', function() {
+
+  });
+
+  //Ingresar datos sobre alumnas activas y desertoras
+  
+};
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
